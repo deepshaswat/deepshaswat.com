09d864f0ba9ca838ec06b01032527a3bcaf4ee1e
Shaswat Deep <deepshaswat@gmail.com>
2025-09-15 18:43:54 +0530

    Shortcut Client issue

--- packages/ui/src/components/web/shortcut/shortcut-button.tsx
+++ packages/ui/src/components/web/shortcut/shortcut-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useKBar } from "kbar";
 import { Button } from "@repo/ui";
 
@@ -14,7 +16,7 @@ export const ShortcutButton = ({
 
   return (
     <Button variant={"ghost"} onClick={query.toggle} className={className}>
-      <div className="grid grid-flow-col gap-1 mx-[0] text-lg">{children}</div>
+      <div className='grid grid-flow-col gap-1 mx-[0] text-lg'>{children}</div>
     </Button>
   );
 };
