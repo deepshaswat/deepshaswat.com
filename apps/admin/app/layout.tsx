0b3f7f7931bdbee2f65bfc6585d855d8a91225d8
Shaswat Deep <deepshaswat@gmail.com>
2025-09-15 14:22:44 +0530

    Issue with config

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         baseTheme: dark,
       }}
     >
-      <html lang='en' suppressHydrationWarning>
+      <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <Providers>{children}</Providers>
         </body>
